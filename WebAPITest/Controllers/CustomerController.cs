91130d351fb892487c1e9ec36e46c19766eb1d4a
Biswaranjan.Rout <Biswaranjan.Rout>
2016-11-04 12:21:59 +0530

    Test

---
+++ WebAPITest/Controllers/CustomerController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPITest.Models;
+
+namespace WebAPITest.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        public static List<Customer> allCustomers = new List<Customer>();
+        
+        public List<Customer> Get()
+        {
+            if(allCustomers.Count == 0)
+            {
+                allCustomers.Add(new Customer
+                {
+                    CustomerID = 1000,
+                    CustomerName = "Customer1"
+                });
+                
+            }
+
+            return allCustomers.ToList();
+        }
+
+        public bool Post(Customer customer)
+        {
+            allCustomers.Add(customer);
+            return true;
+        }
+    }
+}
---
+++ WebAPITest/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPITest.Models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string CustomerName { get; set; }
+    }
+}
